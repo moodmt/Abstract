aea4f30a1f852e9761d6d4dac1a4824e019d009d
Mdmt <mohammadroein4@gmail.com>
2025-05-08 15:33:01 +0200

    +entire file

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import Library.Bug;
+import Library.LivingBeing;
+// package Library;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");
+        LivingBeing bug0 = new Bug("Bug 0");
+        LivingBeing bug1 = new Bug("Bug 1");
+
+        bug0.interact(bug1);
+        
+
+    }
+}
